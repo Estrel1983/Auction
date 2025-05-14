66425fda0d42eb3700550b87d05be7d0f6eea4cc
Yury Shmyrov <estrel1983@gmail.com>
2025-05-13 23:04:42 -0300

    Artist - added Get&Post

---
+++ src/main/java/com/auc/auction/model/Artist.java
@@ -0,0 +1,18 @@
+package com.auc.auction.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.ToString;
+
+@ToString
+@Data
+@Entity
+@Table(name="artist", uniqueConstraints = @UniqueConstraint(columnNames = "artist_name"))
+public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @Column(nullable = false)
+    String artist_name;
+}
